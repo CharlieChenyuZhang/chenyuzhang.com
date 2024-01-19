6fdbf0ad19071973323976aa2ccf0401acd1027f
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2024-01-18 22:46:42 -0500

    working locally

--- server/app.js
+++ server/app.js
@@ -5,9 +5,9 @@ const path = require("path");
 const app = express();
 app.use(cors()); // This will enable CORS for all routes
 const matter = require("gray-matter");
-const port = 3001;
+const port = 8080;
 
-app.get("/api/blog/all", (req, res) => {
+app.get("/blog/all", (req, res) => {
   const mdDirectory = path.join(__dirname, "blogs"); // Adjust the path as necessary
 
   fs.readdir(mdDirectory, (err, files) => {
@@ -39,6 +39,27 @@ app.get("/api/blog/all", (req, res) => {
         }
       });
     });
+  });
+});
+
+app.get("/blog/:name", (req, res) => {
+  const postName = req.params.name;
+  const mdDirectory = path.join(__dirname, "blogs"); // Adjust the path as necessary
+  const filePath = path.join(mdDirectory, postName); // Assuming the files have '.md' extension
+
+  fs.readFile(filePath, "utf8", (err, content) => {
+    if (err) {
+      console.error("Error reading file: ", err);
+      return res.status(500).send({ error: `Error reading file: ${postName}` });
+    }
+
+    const parsed = matter(content);
+
+    if (parsed?.data?.published) {
+      res.send(parsed);
+    } else {
+      res.status(404).send({ error: "Post not found or not published" });
+    }
   });
 });
 
